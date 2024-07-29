b86cd655772517df78fe58c736c4ca15f94f4a01
waisaed <waisaed@umich.edu>
2024-07-29 10:47:46 -0400

    fix edit and footer github link

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -136,7 +136,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/galahq/gala",
+          editUrl: "https://github.com/galahq/gala-docs",
         },
         blog: {
           feedOptions: {
