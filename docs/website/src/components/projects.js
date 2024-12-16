381c76a039d2f5c42600ad59b5e157879244ab5d
waisaed <waisaed@umich.edu>
2024-12-16 15:04:28 -0500

    fix projects responsive layout

--- docs/website/src/components/projects.js
+++ docs/website/src/components/projects.js
@@ -2,15 +2,8 @@ import React from "react";
 import Card from "./card";
 import projects from './projects.module.css'
 
-function columns(number) {
-    switch (number) {
-        case 2:
-            return { gridTemplateColumns: "repeat(2, 1fr)" };
-        case 3:
+function columns() {
             return { gridTemplateColumns: "repeat(3, 1fr)" };
-        default:
-            return { gridTemplateColumns: "repeat(3, 1fr)" };
-    }
 }
 
 export default function Projects({ projectExamples, showTitle, numberOfColumns }) {
@@ -25,11 +18,9 @@ export default function Projects({ projectExamples, showTitle, numberOfColumns }
         <div className="container">
             {!!showTitle && <h3>In Action</h3>}
             <div className="row">
-                <div className="col">
-                    <ul style={columns(numberOfColumns)} className={projects.projectsGrid}>
+                    <ul className={projects.projectsGrid}>
                         {currentProjects}{oldProjects}
                     </ul>
-                </div>
             </div>
         </div >
     );
