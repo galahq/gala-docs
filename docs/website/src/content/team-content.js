36542c7d78752c268cf905d99c92b85ac9ae4dc5
Ed Waisanen <ewaisane@nd.edu>
2025-11-13 15:20:10 -0500

    team page tweaks

--- docs/website/src/content/team-content.js
+++ docs/website/src/content/team-content.js
@@ -3,40 +3,33 @@
 const teamContent = [
     {
         name: "Rebecca Hardin",
-        title: "Principle Investigator",
-        about: `Dr. Hardin is an associate professor at University of Michigan School of Environmental Sciences,
-        the Faculty Director for Justice, Equity, Diversity and Inclusion, as well as director of Gala. 
+        title: "Director",
+        about: `Dr. Hardin is Professor at Professor of Sustainable Development at the Notre Dame Keough School of Global Affairs and director of Gala. 
         She is responsible for setting our organization’s goals, and crafting the big-picture pan for our content 
         and collaborations. Rebecca is passionate about making sustainability education more accessible through 
         the Gala platform.`,
         image: "rebecca.jpeg",
-        website: "",
+        website: "https://keough.nd.edu/about/faculty-staff-directory/rebecca-hardin/",
         linkedIn: "https://www.linkedin.com/in/rebecca-hardin-82686813/",
     },
     {
         name: "Ed Waisanen",
-        title: "Platform and Program Manager",
-        about: "Ed oversees the ongoing development of the Gala platform and advises our community of users on instructional design, multimedia production and use, and the development of interactive data tools. Ed is interested in ways that digital tools can aid the development and use of open educational resources.",
+        title: "Product and Instructional Design Lead",
+        about: "Ed oversees the ongoing development of the Gala platform and advises our community of users on instructional design, multimedia production and use, and the development of interactive data tools. He is interested in ways that digital tools can aid the development and use of Open Educational Resources.",
         image: "ed.png",
         linkedIn: "https://www.linkedin.com/in/ed-waisanen/",
         website: "",
     },
     {
-        name: "Julien DePauw",
-        title: "Director of Online Learning and Academic Innovation",
-        about: `Julien DePauw is a Ph.D. candidate studying Business Model Innovation in Higher Education at the University of Michigan. He helps develop online strategy with Gala in order to make sustainability education more accessible.
-        Julien is passionate about supporting sustainability education through innovative business strategy.`,
-        image: "juliend.png",
-    },
-    {
         name: "Deepika Ganesh",
         title: "Research Assistant",
-        about: "Deepika is a Ph.D. student at the University of Michigan specializing in Education for Sustainable Development (ESD). She is responsible for designing classroom applications for content on the Gala platform. Deepika is passionate about facilitating meaningful experiences engaging with sustainability challenges.",
+        about: "Deepika is a learning designer passionate about sustainability education. She is a Ph.D. Candidate at the School for the Environment, University of Michigan, where her dissertation research explores pedagogical pathways that help STEM learners understand sustainability issues by grounding the development of technology in its social context, drawing from their own experiences or 'sense of place' and using open education - Gala - as a conduit. ",
         image: "deepika.png",
+        website: "https://seas.umich.edu/research/phd-profiles/deepika-ganesh",
     },
     {
         name: "Abigail Merolle",
-        title: "Research Assistant",
+        title: "Program Manager",
         about: "Abigail is a masters student pursuing Environmental Justice at the University of Michigan. In her role at Gala, she specializes in data on water systems, and is currently contributing to the work on CO-DOWN (the Consortium On Disinfection by-products and Opportunistic pathogens in Water Networks). Abigail is passionate about sustainability and how society grapples with adaptation to climate change.",
         image: "abigail.png",
     },
